7cab4c15f307f89cb391c812ddfa29f610645274
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-11-16 11:16:02 -0300

    Adiciona tela de visualização de produtos pelo cliente

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ProdutosCliente from '@/views/ProdutosCliente'
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/Produtos',
+      name: 'Produtos',
+      component: ProdutosCliente
+    }
   ]
 })
---
+++ src/views/ProdutosCliente.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'ProdutosCliente'
+}
+</script>
+
+<style scoped>
+
+</style>
