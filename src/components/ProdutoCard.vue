1b642636cd9dcfe21118e3c62267f260c513a160
Daniel Rodrigues Coura <danielrcifpb@gmail.com>
2018-11-16 12:10:30 -0300

    Cria componente de card dos produtos

---
+++ src/components/ProdutoCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="produto-card">
+    <div class="nome">{{produto.nome}}</div>
+    <div class="fabricante">by {{produto.fabricante}}</div>
+    <div class="preco">{{precoProcessado}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProdutoCard',
+  props: {
+    produto: Object
+  },
+  computed: {
+    precoProcessado () {
+      return `R\$ ${this.produto.preco.toFixed(2)}`
+    }
+  }
+}    
+</script>
+
+<style lang="scss" scoped>
+.produto-card {
+  border: 1px solid #eee;
+  padding: 1rem;
+  font-size: 1.3rem;
+  width: 23%;
+}
+.nome {
+  color: #06b1d3;
+}
+.fabricante {
+  color: #999;
+  font-size: .9rem;
+}
+.preco {
+  font-size: 1.3rem;
+  color: #555;
+  margin-top: 1rem;
+}
+</style>
--- src/views/ProdutosCliente.vue
+++ src/views/ProdutosCliente.vue
@@ -1,13 +1,61 @@
 <template>
-  <div></div>
+  <div>
+    <div class="produtos">
+      <produto-card v-for="(produto, index) in produtos" :key="index" :produto="produto"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import ProdutoCard from '@/components/ProdutoCard'
+
 export default {
-  name: 'ProdutosCliente'
+  name: 'ProdutosCliente',
+  data () {
+    return {
+      produtos: [
+        {
+          nome: 'Produto1',
+          categoria: 'Medicamentos',
+          fabricante: 'Fab1',
+          disponivel: false,
+          preco: 10.00
+        },
+        {
+          nome: 'Produto2',
+          categoria: 'Higiene pessoal',
+          fabricante: 'Fab2',
+          disponivel: true,
+          preco: 20.00
+        },
+        {
+          nome: 'Produto3',
+          categoria: 'Cosméticos',
+          fabricante: 'Fab3',
+          disponivel: true,
+          preco: 30.00
+        },
+        {
+          nome: 'Produto4',
+          categoria: 'Alimentos',
+          fabricante: 'Fab4',
+          disponivel: false,
+          preco: 40.00
+        }
+      ]
+    }
+  },
+  components: {
+    ProdutoCard
+  }
 }
 </script>
 
 <style scoped>
-
+.produtos {
+  display: flex;
+  justify-content: space-around;
+  width: 80%;
+  margin: auto;
+}
 </style>
