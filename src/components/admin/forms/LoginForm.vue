945ac001a4d79ae464ef1ee90baaa06289f6205f
anderson.vidal <anderson.vidal@virtus.ufcg.edu.br>
2018-11-29 15:44:38 -0300

    loginform

---
+++ src/components/admin/forms/LoginForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <input type="password" name="password" v-model="input.password" placeholder="Password" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script scoped>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username or password or both is incorrect");
+                    }
+                } else {
+                    console.log("The username end password is a necessary");
+                }
+            }
+        }
+    }
+</script>
+
+
