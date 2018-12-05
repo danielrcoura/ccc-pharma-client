48569ad9bc8aa3b3057a09eaf8fc5e167a086af8
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-12-04 21:30:34 -0300

    Calcula a quantidade de lotes no dashboard

---
+++ src/components/admin/EstoqueInfo.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="content">
+    <div>Lotes válidos: {{ lotesInfo.numLotes }}</div>
+    <div>Produtos: {{ lotesInfo.numItems }}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'EstoqueInfo',
+  computed: {
+    ...mapState(['lotes']),
+    lotesInfo () {
+      let numLotes = 0
+      let numItems = 0
+      const listLotes = Object.values(this.lotes)
+      listLotes.forEach(lote => {
+        const loteDate = new Date(lote.validade)
+        if (lote.quantidade > 0 && loteDate >= Date.now()) {
+          numLotes++
+          numItems += lote.quantidade
+        }
+      })
+
+      return {numLotes, numItems}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@keyframes fade {
+  0% {
+    opacity: 0;
+    transform: translateY(10px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+}
+
+.content {
+  align-items: center;
+  color: #666;
+  text-align: center;
+  margin: 0 5rem;
+  animation: fade .5s 1;
+  & > * {
+    padding: .5rem;
+    &:first-child {
+      border-bottom: 1px solid #e1e1e1;
+    }
+  }
+}
+</style>
