975a742acd99dbcb555f5a0ea322325344513104
izaquielnn <izaquiel.nascimento@ccc.ufcg.edu.br>
2018-12-07 00:09:49 -0300

    estiliza pesquisa de produtos

--- src/components/admin/forms/SaleForm.vue
+++ src/components/admin/forms/SaleForm.vue
@@ -3,14 +3,18 @@
     <div class="modal-content">
 
       <div class="search-container">
-        <button class="search">
+        <div class="search">
           <input type="text" v-model="search" placeholder="Pesquisar produto"/>
           <span>{{icons.magnifyingGlass}}</span>
-        </button>
+        </div>
         
         <ul class="product-list">
           <li v-for="(produto, id) in filteredProducts" :key="id">
-            {{produto.nome}}
+            
+              <p class="product">{{produto.nome}}</p>
+              <p class="preco">R$ {{produto.preco}}</p>
+              <p class="seta"> >> </p>
+            
           </li>
         </ul>      
       </div>
@@ -157,6 +161,70 @@ export default {
 
 <style lang='scss' scoped>
 @import 'src/assets/css/forms.scss';
+
+.search {
+
+  border-radius: 15px;
+  border: 0.05rem solid #999;
+  padding: 5px;
+  text-align: right;
+  display: flex;
+
+  input {
+    margin-left: 1rem;
+    border: none;
+    outline: none;
+    font-size: 1rem;
+    width: 100%;
+    color: #333;
+    
+  }
+
+
+}
+
+.product-list {
+  list-style: none;
+  margin-top: 10px;
+  color: #333;
+  max-height: 60vh;
+  overflow-x: auto;
+  border: 1px solid #999;
+  border-radius: 10px;
+  li {
+
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 3rem;
+    border-bottom: 1px solid #999;
+    
+    cursor: pointer;
+    
+    &:hover{
+      background-color: #f1f1f1;
+      .seta {
+          background-color: #06b1d3;
+          border-radius: 10px;
+      }
+    }
+
+    p {
+      display: inline-block;
+      padding: 10px;
+    }
+
+    .preco {
+      color: #999;
+    }
+
+    .seta {
+      color: white;
+    }
+    
+  }
+}
+
 </style>
 
 
